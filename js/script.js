b972b97a6a749b7da6ecb25cadf8bafbaf82f62b
dav1312 <63931154+dav1312@users.noreply.github.com>
2022-11-25 17:12:16 +0100

    Prettier

--- js/script.js
+++ js/script.js
@@ -1,46 +1,48 @@
-document.getElementById('fenbox').value = ''
+document.getElementById("fenbox").value = "";
 
 var board,
-game = new Chess(),
-fenEl = $('#fen');
-pgnEl = $('#pgn');
-var $board = $('#board');
+  game = new Chess(),
+  fenEl = $("#fen");
+pgnEl = $("#pgn");
+var $board = $("#board");
 var squareToHighlight = null;
-var squareClass = 'square-55d63';
+var squareClass = "square-55d63";
 
 const apiUrl = "https://www.chessdb.cn/cdb.php";
 
 setPgnGameHeader();
 
 function removeHighlights() {
-  $board.find(`.${squareClass}`).removeClass('highlight-sq');
+  $board.find(`.${squareClass}`).removeClass("highlight-sq");
 }
 
 function addHighlights(source, target) {
-  $board.find(`.square-${source}`).addClass('highlight-sq');
-  $board.find(`.square-${target}`).addClass('highlight-sq');
+  $board.find(`.square-${source}`).addClass("highlight-sq");
+  $board.find(`.square-${target}`).addClass("highlight-sq");
 }
 
 // Disable picking of pieces if the game is over. Also disable picking
 // of pieces for the side not to move.
-var onDragStart = function(source, piece, position, orientation) {
-  if (game.game_over() === true ||
-      (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-      (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+var onDragStart = function (source, piece, position, orientation) {
+  if (
+    game.game_over() === true ||
+    (game.turn() === "w" && piece.search(/^b/) !== -1) ||
+    (game.turn() === "b" && piece.search(/^w/) !== -1)
+  ) {
     return false;
   }
 };
 
-var onDrop = function(source, target) {
+var onDrop = function (source, target) {
   // see if the move is legal
   var move = game.move({
     from: source,
     to: target,
-    promotion: 'q' // NOTE: always promote to a queen for example simplicity
+    promotion: "q", // NOTE: always promote to a queen for example simplicity
   });
 
   // illegal move
-  if (move === null) return 'snapback';
+  if (move === null) return "snapback";
 
   // Hight the last move made
   removeHighlights();
@@ -50,22 +52,22 @@ var onDrop = function(source, target) {
 };
 
 function onMoveEnd() {
-  $board.find(`.square-${squareToHighlight}`).addClass('highlight-sq');
+  $board.find(`.square-${squareToHighlight}`).addClass("highlight-sq");
 }
 
 // update the board position after the piece snap
 // for castling, en passant, pawn promotion
-var onSnapEnd = function() {
+var onSnapEnd = function () {
   board.position(game.fen());
 };
 
 var cfg = {
   draggable: true,
-  position: 'start',
+  position: "start",
   onDragStart: onDragStart,
   onDrop: onDrop,
   onMoveEnd: onMoveEnd,
-  onSnapEnd: onSnapEnd
+  onSnapEnd: onSnapEnd,
 };
 
 function doMove(move) {
@@ -74,7 +76,7 @@ function doMove(move) {
 
   // Remove square highlight of last move.
   removeHighlights();
-  
+
   // Restore square highlight of last last move.
   moveHistory = game.history({ verbose: true });
   console.log(moveHistory);
@@ -90,14 +92,14 @@ function requestQueue() {
   $.get(`${apiUrl}?action=queue&board=${game.fen()}`);
   console.log("FEN requested");
   updateStatus();
-};
+}
 
 function displayScore(score) {
   if (game.turn() === "b") score *= -1;
   if (score > 20000) return `White wins in ${30000 - score}`;
   if (score < -20000) return `Black wins in ${30000 + score}`;
-  return score > 0 ? `+${(score / 100).toFixed(2)}` : (score / 100).toFixed(2)
-};
+  return score > 0 ? `+${(score / 100).toFixed(2)}` : (score / 100).toFixed(2);
+}
 
 // Query leaf score of top X move.
 // Get the top move, push it, and query its PV. Walk the PV except the
@@ -106,12 +108,11 @@ function queryLeaf(data, numPv) {
   var idStr = `advance-pv${numPv}`;
   var label = `Pv ${numPv}: `;
   document.getElementById(idStr).innerHTML = label;
-  if (typeof(data.moves) === 'undefined') {
-    console.log('Query all, there is no book move!');
-  }
-  else {
+  if (typeof data.moves === "undefined") {
+    console.log("Query all, there is no book move!");
+  } else {
     var json = data.moves;
-    for (var j = numPv-1; j < Math.min(numPv, json.length); j++) {
+    for (var j = numPv - 1; j < Math.min(numPv, json.length); j++) {
       var topSanMove = json[j].san;
       var topGame = new Chess(game.fen());
       topGame.move(topSanMove);
@@ -119,37 +120,42 @@ function queryLeaf(data, numPv) {
 
       // Query the top pv, walk the pv and get its leaf score.
       var topUrl = `${apiUrl}?action=querypv&json=1&board=${topFen}`;
-      $.get(topUrl, function(topData, topStatus) {
-        if (topStatus == 'success' && topData.status == 'ok'){
+      $.get(topUrl, function (topData, topStatus) {
+        if (topStatus == "success" && topData.status == "ok") {
           var game1 = new Chess(topGame.fen());
           var depth = topData.depth - 1;
           if (depth >= 0) {
             for (var i = 0; i < depth; i++) {
-                game1.move(topData.pvSAN[i]);
+              game1.move(topData.pvSAN[i]);
             }
             var leafFen = game1.fen();
             var url1 = `${apiUrl}?action=querypv&json=1&board=${leafFen}`;
-            $.get(url1, function(data1, status1) {
-              if (status1 == 'success' && data1.status == 'ok'){
+            $.get(url1, function (data1, status1) {
+              if (status1 == "success" && data1.status == "ok") {
                 score = data1.score;
                 if (game.turn() !== game1.turn()) {
-                  score = -1*score;
+                  score = -1 * score;
                 }
-                var leafNodeInfo = `Eval of <b>${json[numPv-1].san}</b> after ${1 + depth} plies: <b>${displayScore(score)}</b>`;
+                var leafNodeInfo = `Eval of <b>${
+                  json[numPv - 1].san
+                }</b> after ${1 + depth} plies: <b>${displayScore(score)}</b>`;
                 document.getElementById(idStr).innerHTML = leafNodeInfo;
               }
             });
           }
-        }
-        else {
-          document.getElementById(idStr).innerHTML = `Eval of <b>${json[numPv-1].san}</b> after ${1} plies: <b>${displayScore(json[numPv-1].score)}</b>`;
+        } else {
+          document.getElementById(idStr).innerHTML = `Eval of <b>${
+            json[numPv - 1].san
+          }</b> after ${1} plies: <b>${displayScore(
+            json[numPv - 1].score
+          )}</b>`;
         }
       });
     }
   }
-};
+}
 
-var probe_book = function() {
+var probe_book = function () {
   var baseUrl = `${apiUrl}?action=queryall&json=1&board=`;
   var pvUrl = `${apiUrl}?action=querypv&json=1&board=`;
 
@@ -162,27 +168,28 @@ var probe_book = function() {
   if (game.game_over()) {
     var msg = "Game over!";
     console.log(msg);
-    document.getElementById('top-move-pv').textContent = msg;
+    document.getElementById("top-move-pv").textContent = msg;
     for (var i = 0; i < 4; i++) {
-      document.getElementById(`advance-pv${i+1}`).textContent = `Pv${i+1}: ${msg}`;
+      document.getElementById(`advance-pv${i + 1}`).textContent = `Pv${
+        i + 1
+      }: ${msg}`;
     }
     $("#tbody tr").remove();
     return;
   }
 
   // (1) Request query all
-  $.get(url, function(data, status) {
-    if (typeof(data.moves) === 'undefined') {
+  $.get(url, function (data, status) {
+    if (typeof data.moves === "undefined") {
       $("#tbody tr").remove();
-    }
-    else {
+    } else {
       var json = data.moves;
 
       // Create table for book probing results
       // Clear table first
       $("#tbody tr").remove();
 
-      var tbody = document.getElementById('tbody');
+      var tbody = document.getElementById("tbody");
 
       for (var i = 0; i < json.length; i++) {
         var sanMove = json[i].san;
@@ -199,58 +206,52 @@ var probe_book = function() {
     }
   });
 
-  $.get(url, function(data, status) {
-    for(let leafCount = 1; leafCount <= 4; leafCount++) {
+  $.get(url, function (data, status) {
+    for (let leafCount = 1; leafCount <= 4; leafCount++) {
       queryLeaf(data, leafCount);
     }
   });
 
   // (2) Request PV of top 1 move and show it in PV box.
-  $.get(pvUrlGet, function(data, status) {
-    if (status !== 'success'){
+  $.get(pvUrlGet, function (data, status) {
+    if (status !== "success") {
       msg = "Request failed! PV query of top 1 move is not successful.";
       console.log(msg);
-      document.getElementById('top-move-pv').textContent = msg;
-    }
-    else if (data.status !== 'ok') {
-      if (!game.game_over()){
+      document.getElementById("top-move-pv").textContent = msg;
+    } else if (data.status !== "ok") {
+      if (!game.game_over()) {
         msg = "Request is successful but PV info is not available.";
         console.log(msg);
-        document.getElementById('top-move-pv').textContent = msg;
+        document.getElementById("top-move-pv").textContent = msg;
+      } else {
+        document.getElementById("top-move-pv").textContent = "Game over!";
       }
-      else {
-        document.getElementById('top-move-pv').textContent = "Game over!";
-      }
-    }
-    else {
+    } else {
       var sanPv = "" + data.pvSAN;
-      var pv = sanPv.replace(/,/g, ' ');
-      var line = `Eval: ${displayScore(data.score)} Depth: ${data.depth}<br>${pv}`;
-      document.getElementById('top-move-pv').innerHTML = line;
+      var pv = sanPv.replace(/,/g, " ");
+      var line = `Eval: ${displayScore(data.score)} Depth: ${
+        data.depth
+      }<br>${pv}`;
+      document.getElementById("top-move-pv").innerHTML = line;
     }
   });
-}
+};
 
 // Alert user if game is over. Probe online book. Show the fen after
 // each move. Update game result, fen and pgn boxes.
-var updateStatus = function() {
-  var moveColor = 'White';
-  if (game.turn() === 'b')
-    moveColor = 'Black';
+var updateStatus = function () {
+  var moveColor = "White";
+  if (game.turn() === "b") moveColor = "Black";
 
   // checkmate?
   if (game.in_checkmate() === true) {
-    if (moveColor === 'Black')
-      game.header('Result', '1-0');
-    else
-      game.header('Result', '0-1');
+    if (moveColor === "Black") game.header("Result", "1-0");
+    else game.header("Result", "0-1");
   }
   // draw?
-  else if (game.in_draw() === true)
-    game.header('Result', '1/2-1/2');
-
+  else if (game.in_draw() === true) game.header("Result", "1/2-1/2");
   else {
-    game.header('Result', '*');
+    game.header("Result", "*");
   }
 
   // Probe the ChessDB online opening book.
@@ -258,58 +259,56 @@ var updateStatus = function() {
 
   // Update the fen html PGN boxes
   fenEl.html(game.fen());
-  pgnEl.html(game.pgn({ max_width: 79, newline_char: '<br />' }));
-
+  pgnEl.html(game.pgn({ max_width: 79, newline_char: "<br />" }));
 }; // End of updateStatus
 
-$('#clearPosBoxBtn').on('click', function() {
-  document.getElementById('fenbox').value = '';
+$("#clearPosBoxBtn").on("click", function () {
+  document.getElementById("fenbox").value = "";
 });
 
-$('#inputEpdBtn').on('click', function() {
-  var pos = document.getElementById('fenbox').value;
+$("#inputEpdBtn").on("click", function () {
+  var pos = document.getElementById("fenbox").value;
 
   // Remove empty space at left/right of fen/epd string. Position copied
   // from Arena 3.5 chess GUI adds empty char at right of fen.
   pos = pos.trim();
-  document.getElementById('fenbox').value = pos;
+  document.getElementById("fenbox").value = pos;
 
   var ok = game.load(pos);
   if (ok) {
     setPgnGameHeader();
     board.position(pos);
     var s = document.getElementById("tbody");
-    s.innerHTML = '';
+    s.innerHTML = "";
 
     removeHighlights();
     updateStatus();
-  }
-  else {
+  } else {
     console.log(`FEN loading is not OK! FEN: ${pos}`);
-    alert('The fen/epd that you set up is illegal!')
+    alert("The fen/epd that you set up is illegal!");
   }
 });
 
-$('#flipBtn').on('click', function(){
+$("#flipBtn").on("click", function () {
   board.flip(true);
 });
 
-$('#startBtn').on('click', function() {
+$("#startBtn").on("click", function () {
   board.start(false);
   var startpos = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
   board.position(startpos);
   game.load(startpos);
-  document.getElementById('fenbox').value = ''
+  document.getElementById("fenbox").value = "";
   removeHighlights();
   setPgnGameHeader();
   updateStatus();
 });
 
-board = ChessBoard('board', cfg);
+board = ChessBoard("board", cfg);
 updateStatus();
 
 // Undo last move
-$('#undoBtn').on('click', function() {
+$("#undoBtn").on("click", function () {
   game.undo();
   board.position(game.fen());
 
@@ -326,39 +325,40 @@ $('#undoBtn').on('click', function() {
   updateStatus();
 });
 
-$('#requestBtn').on('click', requestQueue);
+$("#requestBtn").on("click", requestQueue);
 
-$('#refreshBtn').on('click', updateStatus);
+$("#refreshBtn").on("click", updateStatus);
 
-function setPgnGameHeader(){
+function setPgnGameHeader() {
   // Get Date for Date game header tag.
   var today = new Date();
   var dd = today.getDate();
   var mm = today.getMonth() + 1; //January is 0!
 
   var yyyy = today.getFullYear();
-  if (dd < 10)
-    dd = '0' + dd;
-  if (mm < 10)
-    mm = '0' + mm;
-  var today = yyyy + '.' + mm + '.' + dd;
+  if (dd < 10) dd = "0" + dd;
+  if (mm < 10) mm = "0" + mm;
+  var today = yyyy + "." + mm + "." + dd;
 
   // Add other header tags
-  game.header('Event', 'ChessDB book probing');
-  game.header('Site', 'Online');
-  game.header('Date', today);
-  game.header('Round', '-');
-  game.header('White', '?');
-  game.header('Black', '?');
-};
+  game.header("Event", "ChessDB book probing");
+  game.header("Site", "Online");
+  game.header("Date", today);
+  game.header("Round", "-");
+  game.header("White", "?");
+  game.header("Black", "?");
+}
 
 // Download game in pgn format.
 function download(filename, text) {
-  var element = document.createElement('a');
-  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-  element.setAttribute('download', filename);
+  var element = document.createElement("a");
+  element.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+  );
+  element.setAttribute("download", filename);
 
-  element.style.display = 'none';
+  element.style.display = "none";
   document.body.appendChild(element);
 
   element.click();
@@ -367,8 +367,12 @@ function download(filename, text) {
 }
 
 // Start file download.
-document.getElementById("savePGNBtn").addEventListener("click", function(){
-  var text = game.pgn({ max_width: 79, newline_char: '\n' }) + "\n\n";
-  var filename = "mygame.pgn";
-  download(filename, text);
-}, false);
+document.getElementById("savePGNBtn").addEventListener(
+  "click",
+  function () {
+    var text = game.pgn({ max_width: 79, newline_char: "\n" }) + "\n\n";
+    var filename = "mygame.pgn";
+    download(filename, text);
+  },
+  false
+);

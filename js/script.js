9e6beef0ca1cb50a78bba5bee562c0394472de1e
dav1312 <63931154+dav1312@users.noreply.github.com>
2022-12-13 08:42:57 +0100

    1

--- js/script.js
+++ js/script.js
@@ -652,3 +652,11 @@ savePGNBtn.addEventListener("click", () => {
   const filename = "mygame.pgn";
   download(filename, text);
 });
+
+let paramString = window.location.href.split('?')[1];
+let queryString = new URLSearchParams(paramString);
+
+for (let pair of queryString.entries()) {
+  console.log("Key is: " + pair[0]);
+  console.log("Value is: " + pair[1]);
+}

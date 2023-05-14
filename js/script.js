0fc456b9a2d4af745984daa86a17b94419f12a64
dav1312 <63931154+dav1312@users.noreply.github.com>
2023-05-14 19:52:25 +0200

    DTZ

--- js/script.js
+++ js/script.js
@@ -193,10 +193,12 @@ const getStats = async () => {
 
 const displayScore = (score) => {
   if (game.turn() === "b") score *= -1;
-  if (score > 25000) return `+M${30000 - score}`;
+  if (score >  25000) return `+M${30000 - score}`;
   if (score < -25000) return `-M${30000 + score}`;
-  if (score > 20000) return `White wins in ${25000 - score}`;
-  if (score < -20000) return `Black wins in ${25000 + score}`;
+  if (score >  20000) return `+DTZ ${25000 - score}`;
+  if (score < -20000) return `-DTZ ${25000 + score}`;
+  if (score >  15000) return `+DTZ ${20000 - score}`;
+  if (score < -15000) return `-DTZ ${20000 + score}`;
   return score > 0 ? `+${(score / 100).toFixed(2)}` : (score / 100).toFixed(2);
 };
 
